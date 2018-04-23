d22f0fc4f6765be903277539394d08fa58a60a64
Isaac Evans <ine@mit.edu>
2018-04-23 11:42:31 -0700

    fix typo

--- chrome/src/inject.js
+++ chrome/src/inject.js
@@ -2,7 +2,7 @@ var project_name_element = document.querySelector('#js-repo-pjax-container > div
 var project_name = project_name_element.attributes.getNamedItem("href").value.substr(1)
 
 var download_button = document.createElement('p');
-download_button.innerHTML = '&nbsp;<a href=' + "https://app.returntocorp.com/reports/repo/" + project_name + '>Secarta Score</a>';
+download_button.innerHTML = '&nbsp;<a href=' + "https://app.returntocorp.com/reports/repos/" + project_name + '>Secarta Score</a>';
 download_button.className = "button special-plugin-button";
 
 injection_element = document.querySelector('#js-repo-pjax-container > div.pagehead.repohead.instapaper_ignore.readability-menu.experiment-repo-nav > div > h1 > span.Label.Label--outline.v-align-middle')
