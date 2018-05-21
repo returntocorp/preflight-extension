62dd061285f00f57e4ab482c673939d28990ef74
Derrick Liu <dliu@lediur.com>
2018-05-21 14:33:32 -0700

    Better score styling

--- src/github/inject.js
+++ src/github/inject.js
@@ -1,114 +1,162 @@
-var project_name_element = document.querySelector(
-  "#js-repo-pjax-container > div.pagehead.repohead.instapaper_ignore.readability-menu.experiment-repo-nav > div > h1 > strong > a"
-);
-var project_name = project_name_element.attributes
-  .getNamedItem("href")
-  .value.substr(1);
+console.log("Injecting");
 
-var badge_html;
+const SECARTA_BADGE_ID = "secarta-score-badge";
+const INJECT_CSS_PATH = "div.repohead-details-container ul.pagehead-actions";
+const REPO_NAME_CSS_PATH =
+  "#js-repo-pjax-container > div.pagehead.repohead.instapaper_ignore.readability-menu.experiment-repo-nav > div > h1 > strong > a";
 
-browser.storage.sync.get(["access_token", "expires_at"], function(res) {
-  var badge_xhr = new XMLHttpRequest();
-  badge_xhr.open(
-    "GET",
-    "https://app.returntocorp.com/api/packages/github.com/" +
-      project_name +
-      "/badge",
-    true
-  );
+var repoNameElem = document.querySelector(REPO_NAME_CSS_PATH);
+var repoName = repoNameElem.attributes.getNamedItem("href").value.substr(1);
 
-  var score_xhr = new XMLHttpRequest();
-  score_xhr.open(
-    "GET",
-    "https://app.returntocorp.com/api/packages/github.com/" +
-      project_name +
-      "/score",
-    true
-  );
-
-  badge_xhr.onload = function(e) {
-    if (badge_xhr.readyState === 4) {
-      if (badge_xhr.status === 200) {
-        var badge = makeBadgeElem();
-        badge.innerHTML = badge_xhr.responseText;
-        injectElem(badge);
-        badge_html = badge.outerHTML;
-
-        // add badge to storage for popup
-        browser.storage.sync.set({ badge_html: badge.outerHTML });
-
-        // also grab score for popup
-        score_xhr.setRequestHeader("Authorization", res.access_token);
-        score_xhr.send(null);
-      } else {
-        handleFailure(badge_xhr.status);
-      }
-    }
-  };
-
-  score_xhr.onload = function(e) {
-    if (score_xhr.readyState === 4) {
-      if (score_xhr.status === 200) {
-        console.log("got responseText:");
-        console.log(score_xhr.responseText);
-
-        browser.storage.sync.set(
-          {
-            project_name: project_name,
-            score: JSON.parse(score_xhr.responseText).result
-          },
-          function() {
-            console.log("set the score in storage.");
+browser.storage.sync.get(["access_token", "expires_at"]).then(res => {
+  console.log("Starting fetch");
+  fetch(buildApiScoreLinkForRepo(repoName), {
+    method: "get",
+    headers: new Headers({
+      Authorization: res.access_token
+    })
+  })
+    .then(response => response.json())
+    .then(response => {
+      console.log("Received score!", response);
+      browser.storage.sync.get("projects").then(projects =>
+        browser.storage.sync.set({
+          projects: {
+            ...projects,
+            repoName: {
+              score: response
+            }
           }
-        );
-      } else {
-        console.error("error getting score breakdown");
-        browser.storage.sync.remove("score");
-      }
-    }
-  };
+        })
+      );
 
-  badge_xhr.setRequestHeader("Authorization", res.access_token);
-  badge_xhr.send(null);
+      return response;
+    })
+    .then(response => tryInjectBadge(response))
+    .catch(err => {
+      console.error("error getting score breakdown", err);
+      browser.storage.sync.remove("score");
+    });
 });
 
-function injectElem(elem) {
-  injection_element = document.querySelector(
-    ".repohead-details-container.clearfix.container > h1"
+function tryInjectBadge(response) {
+  if (document.querySelector(`#${SECARTA_BADGE_ID}`) == null) {
+    const injectSite = document.querySelector(INJECT_CSS_PATH);
+    const payload = buildContainer(response);
+    injectSite.insertAdjacentElement("afterbegin", payload);
+  }
+}
+
+/**
+ *
+ * @param {*} response
+ * @returns {HTMLUListElement}
+ */
+function buildContainer(response) {
+  const container = document.createElement("li");
+
+  container.id = SECARTA_BADGE_ID;
+  container.appendChild(buildButton(response));
+  container.appendChild(buildCount(response));
+
+  return container;
+}
+
+/**
+ *
+ * @param {*} response
+ * @returns {HTMLAnchorElement}
+ */
+function buildButton(response) {
+  const githubClasses = ["btn", "btn-sm", "btn-with-count"];
+  const secartaClasses = ["secarta-injected-button", "secarta-score-button"];
+
+  const link = buildElemWithClasses(
+    "a",
+    [...githubClasses, ...secartaClasses],
+    "Secarta"
   );
-  injection_element.insertAdjacentElement("afterEnd", elem);
-  console.log("injected into");
-  console.log(injection_element);
+
+  link.setAttribute("href", buildReportLinkForRepo(repoName));
+
+  return link;
 }
 
-function makeBadgeElem() {
-  var badge = document.createElement("a");
-  badge.href = `https://app.returntocorp.com/reports/repos/github.com/${project_name}`;
-  badge.className = "button special-plugin-button";
-  badge.style.marginLeft = "10px";
-  return badge;
+/**
+ * @param {*} response
+ * @returns {HTMLAnchorElement}
+ */
+function buildCount(response) {
+  const githubClasses = ["social-count"];
+  const secartaClasses = ["secarta-injected-count", "secarta-score-count"];
+
+  return buildElemWithClasses(
+    "a",
+    [...githubClasses, ...secartaClasses],
+    response.result != null ? response.result.score : "?"
+  );
 }
 
-// unsafe!
-function makeFailureElem(text) {
-  var elem = document.createElement("div");
-  elem.style.marginLeft = "10px";
-  elem.innerHTML = text;
+/**
+ * Build an HTML element with the specified classes
+ * @param {string} elem
+ * @param {string[]} classes
+ * @param {string} textContent
+ * @returns {HTMLElement}
+ */
+function buildElemWithClasses(tag, classes, textContent) {
+  const elem = document.createElement(tag);
+  elem.className = classes.join(" ");
+  elem.innerText = textContent;
   return elem;
 }
 
-function handleFailure(status) {
-  var elem;
-  if (status === 401) {
-    console.error(
-      "You need to be logged in to see the Secarta badge for this project"
-    );
-    elem = makeFailureElem("Badge fetch error. Log in?");
-  } else {
-    console.error("Badge fetch error. Analyzed?");
-    elem = makeFailureElem("Badge fetch error. Analyzed?");
-  }
-
-  browser.storage.sync.remove(["score", "project_name"]);
-  injectElem(elem);
+/**
+ *
+ * @param {string} repoName
+ * @returns {string}
+ */
+function buildReportLinkForRepo(repoName) {
+  return `https://app.returntocorp.com/reports/github.com/${repoName}`;
 }
+
+/**
+ *
+ * @param {string} repoName
+ * @returns {string}
+ */
+function buildApiScoreLinkForRepo(repoName) {
+  return `https://app.returntocorp.com/api/packages/github.com/${repoName}/score`;
+}
+
+// function makeBadgeElem() {
+//   var badge = document.createElement("a");
+//   badge.href = `https://app.returntocorp.com/reports/github.com/${repoName}`;
+//   badge.className = "button special-plugin-button";
+//   badge.style.marginLeft = "10px";
+//   return badge;
+// }
+
+// // unsafe!
+// function makeFailureElem(text) {
+//   var elem = document.createElement("div");
+//   elem.style.marginLeft = "10px";
+//   elem.innerHTML = text;
+//   return elem;
+// }
+
+// function handleFailure(status) {
+//   var elem;
+//   if (status === 401) {
+//     console.error(
+//       "You need to be logged in to see the Secarta badge for this project"
+//     );
+//     elem = makeFailureElem("Badge fetch error. Log in?");
+//   } else {
+//     console.error("Badge fetch error. Analyzed?");
+//     elem = makeFailureElem("Badge fetch error. Analyzed?");
+//   }
+
+//   browser.storage.sync.remove(["score", "repoName"]);
+//   injectElem(elem);
+// }
