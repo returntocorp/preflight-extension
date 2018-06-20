dbad83c0e70166d51394310645785e0d0820633d
Luke O'Malley <luke@returntocorp.com>
2018-06-19 17:42:08 -0700

    cleanup code, factor our constants

--- src/github/inject.js
+++ src/github/inject.js
@@ -6,10 +6,14 @@ const SECARTA_BADGE_ID = "secarta-score-badge";
 const INJECT_CSS_PATH = "div.repohead-details-container ul.pagehead-actions";
 const REPO_NAME_CSS_PATH =
   "#js-repo-pjax-container > div.pagehead.repohead.instapaper_ignore.readability-menu.experiment-repo-nav > div > h1 > strong > a";
+
 const SECARTA_URL = "https://app.secarta.io";
 
 const SHIELD_ICON = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 0c-3.371 2.866-5.484 3-9 3v11.535c0 4.603 3.203 5.804 9 9.465 5.797-3.661 9-4.862 9-9.465v-11.535c-3.516 0-5.629-.134-9-3zm-7 14.535v-2.535h7v-9.456c2.5 1.805 4.554 2.292 7 2.416v7.04h-7v9.643c-5.31-3.278-7-4.065-7-7.108z"/></svg>`;
 const LOCK_ICON = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M18 10v-4c0-3.313-2.687-6-6-6s-6 2.687-6 6v4h-3v14h18v-14h-3zm-4.138 9.975l-1.862-1.836-1.835 1.861-1.13-1.129 1.827-1.86-1.862-1.837 1.129-1.13 1.859 1.827 1.838-1.871 1.139 1.139-1.833 1.86 1.868 1.836-1.138 1.14zm-5.862-9.975v-4c0-2.206 1.795-4 4-4s4 1.794 4 4v4h-8z"/></svg>`;
+
+const ACCESS_TOKEN_KEY = "access_token";
+const EXPIRES_AT_KEY = "expires_at";
 
 const REPO_NAME_ELEM = document.querySelector(REPO_NAME_CSS_PATH);
 const REPO_NAME = REPO_NAME_ELEM.attributes
@@ -29,18 +33,20 @@ function getBrowserStorage(param) {
 
 function fetchAndInjectBadge() {
   if (document.getElementById(SECARTA_BADGE_ID) == null) {
-    getBrowserStorage(["access_token", "expires_at"])
+    getBrowserStorage([ACCESS_TOKEN_KEY, EXPIRES_AT_KEY])
       .then(storage => {
-        const expiresAt = storage["expires_at"];
-        if (storage["access_token"] == null || expiresAt == null) {
+        if (
+          storage[ACCESS_TOKEN_KEY] == null ||
+          storage[EXPIRES_AT_KEY] == null
+        ) {
           throw new Error("missing Secarta access_token, log in to Secarta");
-        } else if (expiresAt < new Date().getTime()) {
+        } else if (storage[EXPIRES_AT_KEY] < new Date().getTime()) {
           throw new Error("expired Secarta access_token");
         }
         return storage;
       })
       .then(storage => {
-        return fetchRepo(REPO_NAME, storage["access_token"]);
+        return fetchRepo(REPO_NAME, storage[ACCESS_TOKEN_KEY]);
       })
       .then(response => tryInjectBadge(buildContainer(response)))
       .catch(err => {
@@ -83,7 +89,7 @@ function buildContainer(response, err) {
   container.className =
     err == null ? getScoreBucket(response) : "secarta-error";
 
-  container.appendChild(buildButton(response));
+  container.appendChild(buildButton());
   container.appendChild(buildCount(response));
 
   return container;
@@ -94,7 +100,7 @@ function buildContainer(response, err) {
  * @param {*} response
  * @returns {HTMLAnchorElement}
  */
-function buildButton(response) {
+function buildButton() {
   const githubClasses = ["btn", "btn-sm", "btn-with-count"];
   const secartaClasses = ["secarta-injected-button", "secarta-score-button"];
 
