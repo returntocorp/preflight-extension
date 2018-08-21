7a10f5ebb029b53c7b3305e30286b7ea97e8b014
Derrick Liu <dliu@lediur.com>
2018-08-21 15:43:58 -0700

    Set uninstall url in background instead

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Guide from "./popup";
 
-browser.runtime.setUninstallUrl("https://goo.gl/forms/B8ALNAcRHoLWHBVF2");
-
 ReactDOM.render(<Guide />, document.getElementById(
   "r2c-popup-root"
 ) as HTMLElement);
