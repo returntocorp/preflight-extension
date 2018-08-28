590a76dd50ce1b433c1764e7ea93dc92399af3ac
Derrick Liu <dliu@lediur.com>
2018-08-28 14:57:25 -0700

    RepoTwist with package info

--- src/content/RepoTwist.tsx
+++ src/content/RepoTwist.tsx
@@ -1,8 +1,25 @@
-import { NonIdealState, Spinner } from "@blueprintjs/core";
+import {
+  Button,
+  ButtonGroup,
+  Classes,
+  InputGroup,
+  NonIdealState,
+  Spinner
+} from "@blueprintjs/core";
 import Fetch from "@lediur/react-fetch-component";
-import { PermissionsResponse, permissionsUrl } from "@r2c/extension/api/permissions";
+import { PackageResponse, packageUrl } from "@r2c/extension/api/package";
+import {
+  PermissionsResponse,
+  permissionsUrl
+} from "@r2c/extension/api/permissions";
 import { scoreRepoUrl, ScoreResponse } from "@r2c/extension/api/score";
+import ProfilePicture from "@r2c/extension/shared/ProfilePicture";
+import {
+  getPreferredPackageManager,
+  setPreferredPackageManager
+} from "@r2c/extension/utils";
 import * as classnames from "classnames";
+import * as copy from "copy-to-clipboard";
 import * as React from "react";
 import "./RepoTwist.css";
 
@@ -19,6 +36,19 @@ const RepoNoData: React.SFC = () => (
     }
     title="Couldn't get repo data"
   />
+);
+
+const Copy: React.SFC = () => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 24 24"
+    fillRule="evenodd"
+    clipRule="evenodd"
+    className="copy-button"
+  >
+    <path d="M20 24h-20v-22h3c1.229 0 2.18-1.084 3-2h8c.82.916 1.771 2 3 2h3v9h-2v-7h-4l-2 2h-3.898l-2.102-2h-4v18h16v-5h2v7zm-10-4h-6v-1h6v1zm0-2h-6v-1h6v1zm6-5h8v2h-8v3l-5-4 5-4v3zm-6 3h-6v-1h6v1zm0-2h-6v-1h6v1zm0-2h-6v-1h6v1zm0-2h-6v-1h6v1zm-1-7c0 .552.448 1 1 1s1-.448 1-1-.448-1-1-1-1 .448-1 1z" />
+  </svg>
 );
 
 const RepoScoreSection: React.SFC = () => (
@@ -75,6 +105,180 @@ const RepoPermissionsSection: React.SFC = () => (
   </Fetch>
 );
 
+type PackageManagerChoice = "npm" | "yarn";
+
+interface RepoPackageSectionState {
+  packageManager: PackageManagerChoice;
+}
+
+class RepoPackageSection extends React.Component<{}, RepoPackageSectionState> {
+  public DEFAULT_PACKAGE_MANAGER: PackageManagerChoice = "npm";
+
+  public state: RepoPackageSectionState = {
+    packageManager: this.DEFAULT_PACKAGE_MANAGER
+  };
+
+  private packageFieldRef: { [key: string]: HTMLInputElement | null } = {};
+
+  public componentDidMount() {
+    this.fetchPreferredPackageManager();
+  }
+
+  public render() {
+    return (
+      <Fetch<PackageResponse> url={packageUrl()}>
+        {({ loading, data, error }) => (
+          <section className="nutrition-packages nutrition-section">
+            <header className="nutrition-title">Packages</header>
+            {loading && (
+              <div className="nutrition-section-value loading">
+                <NonIdealState icon={<Spinner />} title="Loading..." />
+              </div>
+            )}
+            {data && (
+              <div className="nutrition-section-value">
+                {data.packages.slice(0, 1).map((entry, i) => (
+                  <div key={i} className="nutrition-package">
+                    <header className="package-header">
+                      <span className="package-name">{entry.name}</span>
+                      {entry.endorsers && (
+                        <div className="package-endorsers">
+                          {entry.endorsers.slice(0, 3).map(endorser => (
+                            <ProfilePicture
+                              key={endorser}
+                              user={endorser}
+                              className="package-endorser"
+                            />
+                          ))}
+                          {entry.endorsers.length > 3 && (
+                            <span className="package-endorsers-more">
+                              +{entry.endorsers.length - 3}
+                            </span>
+                          )}
+                        </div>
+                      )}
+                    </header>
+
+                    <InputGroup
+                      type="text"
+                      value={this.buildInstallCommand(
+                        this.state.packageManager,
+                        entry.name
+                      )}
+                      rightElement={
+                        <Button
+                          icon={<Copy />}
+                          minimal={true}
+                          onClick={this.handleCopy(
+                            this.state.packageManager,
+                            entry.name
+                          )}
+                        />
+                      }
+                      className={classnames(Classes.FILL, "package-npm-field")}
+                      inputRef={this.setPackageFieldRef(
+                        this.state.packageManager,
+                        entry.name
+                      )}
+                      readOnly={true}
+                    />
+
+                    <ButtonGroup className={Classes.FILL}>
+                      <Button
+                        active={this.state.packageManager === "npm"}
+                        onClick={this.handleRegistryChange("npm")}
+                      >
+                        NPM
+                      </Button>
+                      <Button
+                        active={this.state.packageManager === "yarn"}
+                        onClick={this.handleRegistryChange("yarn")}
+                      >
+                        Yarn
+                      </Button>
+                    </ButtonGroup>
+                  </div>
+                ))}
+                {data.packages.length > 1 && (
+                  <div className="nutrition-package-more">
+                    and {data.packages.length - 1} more
+                  </div>
+                )}
+              </div>
+            )}
+          </section>
+        )}
+      </Fetch>
+    );
+  }
+
+  private fetchPreferredPackageManager = async () => {
+    const packageManager = (await getPreferredPackageManager()) as PackageManagerChoice;
+    if (packageManager != null) {
+      this.setState({
+        packageManager: packageManager || this.DEFAULT_PACKAGE_MANAGER
+      });
+    }
+  };
+
+  private handleRegistryChange = (packageManager: PackageManagerChoice) => (
+    e: React.MouseEvent<HTMLElement>
+  ) => {
+    this.setState({ packageManager });
+    setPreferredPackageManager(packageManager);
+  };
+
+  private handleCopy = (registry: string, packageName: string) => (
+    e: React.MouseEvent<HTMLElement>
+  ) => {
+    const key = this.buildPackageFieldRefKey(registry, packageName);
+    const toCopy = this.packageFieldRef[key];
+
+    console.log(this.packageFieldRef);
+
+    if (toCopy != null) {
+      toCopy.focus();
+      toCopy.setSelectionRange(0, toCopy.value.length);
+      this.copyToClipboard(this.buildInstallCommand(registry, packageName));
+    }
+  };
+
+  private copyToClipboard = (toCopy: string) => {
+    return copy(toCopy);
+  };
+
+  private setPackageFieldRef = (registry: string, packageName: string) => (
+    ref: HTMLInputElement | null
+  ) => {
+    const key = this.buildPackageFieldRefKey(registry, packageName);
+    this.packageFieldRef[key] = ref;
+  };
+
+  private buildPackageFieldRefKey = (
+    registry: string,
+    packageName: string
+  ): string => `${registry}/${packageName}`;
+
+  private buildInstallCommand = (
+    registry: string,
+    packageName: string,
+    dev: boolean = false
+  ): string => {
+    const cmds = {
+      npm: `npm install ${dev ? "-D " : ""}${packageName}`,
+      yarn: `yarn add ${dev ? "-D " : ""}${packageName}`
+    };
+    switch (registry) {
+      case "npm":
+        return cmds.npm;
+      case "yarn":
+        return cmds.yarn;
+      default:
+        return cmds.npm;
+    }
+  };
+}
+
 export default class RepoTwist extends React.Component<RepoTwistProps> {
   public render() {
     const { repoSlug } = this.props;
@@ -93,6 +297,7 @@ export default class RepoTwist extends React.Component<RepoTwistProps> {
             <div className="twist-body">
               <RepoScoreSection />
               <RepoPermissionsSection />
+              <RepoPackageSection />
             </div>
           </>
         )}
