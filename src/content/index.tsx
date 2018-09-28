58e340e54eb8c5608a1c56e4bfad9c71b9dcb489
Derrick Liu <dliu@lediur.com>
2018-09-28 13:13:29 -0700

    Move icons

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -26,28 +26,9 @@ import {
   isRepositoryPrivate
 } from "@r2c/extension/utils";
 import * as React from "react";
+import { PlaneIcon, SmileyIcon, SpeechBubblesIcon } from "../icons";
 import { PreflightChecklistItemType } from "./headsup/PreflightChecklist";
 import "./index.css";
-
-const DiscussionIcon: React.SFC = () => {
-  return (
-    <svg width="24" height="24" viewBox="0 0 24 24">
-      <path d="M2.001 9.352c0 1.873.849 2.943 1.683 3.943.031 1 .085 1.668-.333 3.183 1.748-.558 2.038-.778 3.008-1.374 1 .244 1.474.381 2.611.491-.094.708-.081 1.275.055 2.023-.752-.06-1.528-.178-2.33-.374-1.397.857-4.481 1.725-6.649 2.115.811-1.595 1.708-3.785 1.661-5.312-1.09-1.305-1.705-2.984-1.705-4.695-.001-4.826 4.718-8.352 9.999-8.352 5.237 0 9.977 3.484 9.998 8.318-.644-.175-1.322-.277-2.021-.314-.229-3.34-3.713-6.004-7.977-6.004-4.411 0-8 2.85-8 6.352zm20.883 10.169c-.029 1.001.558 2.435 1.088 3.479-1.419-.258-3.438-.824-4.352-1.385-.772.188-1.514.274-2.213.274-3.865 0-6.498-2.643-6.498-5.442 0-3.174 3.11-5.467 6.546-5.467 3.457 0 6.546 2.309 6.546 5.467 0 1.12-.403 2.221-1.117 3.074zm-7.563-3.021c0-.453-.368-.82-.82-.82s-.82.367-.82.82.368.82.82.82.82-.367.82-.82zm3 0c0-.453-.368-.82-.82-.82s-.82.367-.82.82.368.82.82.82.82-.367.82-.82zm3 0c0-.453-.368-.82-.82-.82s-.82.367-.82.82.368.82.82.82.82-.367.82-.82z" />
-    </svg>
-  );
-};
-
-const PreflightIcon: React.SFC = () => (
-  <svg width="24" height="24" fillRule="evenodd" clipRule="evenodd">
-    <path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 2c5.519 0 10 4.481 10 10s-4.481 10-10 10-10-4.481-10-10 4.481-10 10-10zm-4.809 11.077l3.627-1.796-3.717-3.17 1.149-.569 6.017 2.031 2.874-1.423c.757-.38 2.009-.278 2.294.296.045.092.065.195.065.306-.002.586-.59 1.381-1.221 1.698l-2.874 1.423-2.031 6.016-1.15.569-.268-4.878-3.626 1.796-.749 1.802-.804.398-.281-2.724-1.996-1.874.804-.399 1.887.498z" />
-  </svg>
-);
-
-const ShareIcon: React.SFC = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24">
-    <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm5.507 13.941c-1.512 1.195-3.174 1.931-5.506 1.931-2.334 0-3.996-.736-5.508-1.931l-.493.493c1.127 1.72 3.2 3.566 6.001 3.566 2.8 0 4.872-1.846 5.999-3.566l-.493-.493zm-9.007-5.941c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5-.672-1.5-1.5-1.5zm7 0c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5-.672-1.5-1.5-1.5z" />
-  </svg>
-);
 
 interface ContentHostState {
   twistTab: TwistId | undefined;
@@ -163,7 +144,7 @@ export default class ContentHost extends React.Component<{}, ContentHostState> {
                   <Twist
                     id="preflight"
                     title="Details"
-                    icon={<PreflightIcon />}
+                    icon={<PlaneIcon />}
                     panel={
                       <PreflightTwist
                         repoSlug={this.repoSlug}
@@ -174,7 +155,7 @@ export default class ContentHost extends React.Component<{}, ContentHostState> {
                   <Twist
                     id="discussion"
                     title="Comments"
-                    icon={<DiscussionIcon />}
+                    icon={<SpeechBubblesIcon />}
                     panel={
                       <Discussion user={user} installationId={installationId} />
                     }
@@ -182,7 +163,7 @@ export default class ContentHost extends React.Component<{}, ContentHostState> {
                   <Twist
                     id="share"
                     title="Share the extension"
-                    icon={<ShareIcon />}
+                    icon={<SmileyIcon />}
                     panel={
                       <ShareSection
                         rtcLink="https://tinyurl.com/r2c-beta"
