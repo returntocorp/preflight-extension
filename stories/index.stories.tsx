b90ba8cbed7485c08222bfc899454914a5547fe0
Derrick Liu <dliu@lediur.com>
2018-10-02 17:41:02 -0700

    Test with Non-Ideal headsup

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -1,19 +1,14 @@
-import { action } from "@storybook/addon-actions";
+import {
+  ErrorHeadsUp,
+  LoadingHeadsUp
+} from "@r2c/extension/content/headsup/NonIdealHeadsup";
+import centered from "@storybook/addon-centered";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 
-storiesOf("Welcome", module).add("to Storybook", () => (
-  <div>Hello world</div>
-));
-
-storiesOf("Button", module)
-  .add("with text", () => (
-    <button onClick={action("clicked")}>Hello button</button>
-  ))
-  .add("with some emoji", () => (
-    <button onClick={action("clicked")}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </button>
+storiesOf("Headsup", module)
+  .addDecorator(centered)
+  .add("Non-ideal, loading", () => <LoadingHeadsUp />)
+  .add("Non-ideal, error, Error()", () => (
+    <ErrorHeadsUp error={new Error("Example error")} />
   ));
