a397453376c0f680706499a2090534044818ba6a
Derrick Liu <dliu@lediur.com>
2018-10-04 17:22:53 -0700

    Rename current index to popup and create new index for web app entrypoint

---
+++ src/web/index.tsx
@@ -0,0 +1,15 @@
+import PreflightLogo from "@r2c/extension/web/PreflightLogo";
+import * as React from "react";
+import "./index.css";
+
+export default class App extends React.PureComponent {
+  public render() {
+    return (
+      <section className="r2c-web-placeholder">
+        <div className="preflight-logo">
+          <PreflightLogo />
+        </div>
+      </section>
+    );
+  }
+}
