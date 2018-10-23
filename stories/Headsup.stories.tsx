f434d86bf780a225e103f188fb6febdedc1af603
Derrick Liu <dliu@lediur.com>
2018-10-23 15:59:38 -0700

    Refactor stories to add RelatedPackages story

---
+++ stories/Headsup.stories.tsx
@@ -0,0 +1,18 @@
+import ExceptionalHeadsUp from "@r2c/extension/content/headsup/ExceptionalHeadsup";
+import {
+  ErrorHeadsUp,
+  LoadingHeadsUp,
+  UnsupportedHeadsUp
+} from "@r2c/extension/content/headsup/NonIdealHeadsup";
+import centered from "@storybook/addon-centered";
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+
+storiesOf("Headsup", module)
+  .addDecorator(centered)
+  .add("Non-ideal, loading", () => <LoadingHeadsUp />)
+  .add("Non-ideal, error, Error()", () => (
+    <ErrorHeadsUp error={new Error("Example error")} />
+  ))
+  .add("Non-ideal, unsupported", () => <UnsupportedHeadsUp />)
+  .add("Exceptional", () => <ExceptionalHeadsUp />);
