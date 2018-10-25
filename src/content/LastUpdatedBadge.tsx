30d6ea645ee1af51a09f24495950395086574baf
Luke O'Malley <luke@returntocorp.com>
2018-10-25 12:35:17 -0700

    Wrap commit tease in domelementloadedwatcher

--- src/content/LastUpdatedBadge.tsx
+++ src/content/LastUpdatedBadge.tsx
@@ -5,6 +5,7 @@ import {
 } from "@r2c/extension/utils";
 import * as React from "react";
 import TimeAgo from "react-timeago";
+import DomElementLoadedWatcher from "./github/DomElementLoadedWatcher";
 import "./LastUpdatedBadge.css";
 
 interface LastUpdatedBadgeProps {
@@ -16,17 +17,10 @@ interface LastUpdatedBadgeProps {
 export default class LastUpdatedBadge extends React.PureComponent<
   LastUpdatedBadgeProps
 > {
+  private COMMIT_TEASE_SHA_SELECTOR = "a.commit-tease-sha";
+
   public render() {
     const { repoSlug, commitHash, lastUpdatedDate } = this.props;
-
-    const isCurrentCommitByRepoSlug =
-      commitHash != null && commitHash === repoSlug.commitHash;
-    // Returns true for any page that has a commit tease sha element (e.g. GitHub project home, a file)
-    const commitTeaseSha = this.getCommitTeaseSha();
-    const isCurrentCommitByCommitTeaseSha =
-      commitHash != null &&
-      commitTeaseSha != null &&
-      commitHash.startsWith(commitTeaseSha);
 
     return (
       <Popover
@@ -44,9 +38,32 @@ export default class LastUpdatedBadge extends React.PureComponent<
         position={Position.BOTTOM}
       >
         <span className="r2c-last-updated-badge">
-          {(isCurrentCommitByRepoSlug || isCurrentCommitByCommitTeaseSha) && (
-            <div className="current-commit">Current commit</div>
-          )}
+          <DomElementLoadedWatcher
+            querySelector={this.COMMIT_TEASE_SHA_SELECTOR}
+          >
+            {({ done }) => {
+              if (done) {
+                const isCurrentCommitByRepoSlug =
+                  commitHash != null && commitHash === repoSlug.commitHash;
+                // Returns true for any page that has a commit tease sha element (e.g. GitHub project home, a file)
+                const commitTeaseSha = this.getCommitTeaseSha();
+                const isCurrentCommitByCommitTeaseSha =
+                  commitHash != null &&
+                  commitTeaseSha != null &&
+                  commitHash.startsWith(commitTeaseSha);
+
+                if (
+                  isCurrentCommitByRepoSlug ||
+                  isCurrentCommitByCommitTeaseSha
+                ) {
+                  return <div className="current-commit">Current commit</div>;
+                }
+              }
+
+              return null;
+            }}
+          </DomElementLoadedWatcher>
+
           <span className="updated-message">
             Code scanned{" "}
             <span className="updated-timeago">
@@ -60,7 +77,7 @@ export default class LastUpdatedBadge extends React.PureComponent<
 
   private getCommitTeaseSha(): string | null {
     const commitHashElement: HTMLElement | null = document.querySelector(
-      "a.commit-tease-sha"
+      this.COMMIT_TEASE_SHA_SELECTOR
     );
 
     return commitHashElement != null ? commitHashElement.innerText : null;
