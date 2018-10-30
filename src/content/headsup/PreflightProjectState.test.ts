71c70a587288346a50caf3e10cb24f1721570d8b
Derrick Liu <dliu@lediur.com>
2018-10-30 13:50:22 -0700

    more tests for flow state checking

--- src/content/headsup/PreflightProjectState.test.ts
+++ src/content/headsup/PreflightProjectState.test.ts
@@ -1,12 +1,18 @@
 import { flowProjectState } from "@r2c/extension/content/headsup";
 import {
   PreflightChecklistErrors,
+  PreflightChecklistFetchData,
+  PreflightChecklistFetchDataResponse,
   PreflightChecklistLoading
 } from "@r2c/extension/content/headsup/PreflightFetch";
 import {
+  COMPLETE,
+  ERROR_API,
+  ERROR_MISSING_DATA,
   ERROR_UNKNOWN,
   LOADING_ALL,
-  LOADING_SOME
+  LOADING_SOME,
+  PARTIAL
 } from "@r2c/extension/content/headsup/PreflightProjectState";
 
 describe("Project state", () => {
@@ -78,6 +84,110 @@ describe("Project state", () => {
       repo: undefined,
       scripts: new Error("some error")
     };
+    expect(flowProjectState({ ...blank, error })).toBe(ERROR_UNKNOWN);
+  });
+
+  it("shows partial data when there's data and some responses have errored out", () => {
+    const error: PreflightChecklistErrors = {
+      every: false,
+      some: true,
+      findings: undefined,
+      pkg: undefined,
+      repo: undefined,
+      scripts: new Error("some error")
+    };
+    const data: PreflightChecklistFetchData = {
+      some: true,
+      every: false,
+      findings: undefined,
+      pkg: undefined,
+      repo: undefined,
+      scripts: { gitUrl: "foo", scripts: [] }
+    };
+    expect(flowProjectState({ ...blank, data, error })).toBe(PARTIAL);
+  });
+
+  it("shows complete data when there's all data, but some responses have errored out", () => {
+    const error: PreflightChecklistErrors = {
+      every: false,
+      some: true,
+      findings: undefined,
+      pkg: undefined,
+      repo: undefined,
+      scripts: new Error("some error")
+    };
+    const data: PreflightChecklistFetchData = {
+      some: true,
+      every: true,
+      findings: { gitUrl: "foo", findings: [], commitHash: "bar" },
+      pkg: { gitUrl: "foo", packages: [] },
+      repo: {
+        gitUrl: "foo",
+        commitHash: "bar",
+        activity: {
+          archived: false,
+          isActive: false,
+          latestCommitDate: "date"
+        },
+        analyzedAt: "analyzed"
+      },
+      scripts: { gitUrl: "foo", scripts: [] }
+    };
+    expect(flowProjectState({ ...blank, data, error })).toBe(COMPLETE);
+  });
+
+  it("shows missing data if all responses are 404 and all errors are triggered", () => {
+    const error: PreflightChecklistErrors = {
+      every: true,
+      some: true,
+      findings: new Error("some error"),
+      pkg: new Error("some error"),
+      repo: new Error("some error"),
+      scripts: new Error("some error")
+    };
+
+    const response: PreflightChecklistFetchDataResponse = {
+      findings: new Response(null, { status: 404 }),
+      pkg: new Response(null, { status: 404 }),
+      repo: new Response(null, { status: 404 }),
+      scripts: new Response(null, { status: 404 })
+    };
+
+    expect(flowProjectState({ ...blank, response, error })).toBe(
+      ERROR_MISSING_DATA
+    );
+  });
+
+  it("shows API issues if not responses are 404 and all errors are triggered", () => {
+    const error: PreflightChecklistErrors = {
+      every: true,
+      some: true,
+      findings: new Error("some error"),
+      pkg: new Error("some error"),
+      repo: new Error("some error"),
+      scripts: new Error("some error")
+    };
+
+    const response: PreflightChecklistFetchDataResponse = {
+      findings: new Response(null, { status: 404 }),
+      pkg: new Response(null, { status: 404 }),
+      repo: new Response(null, { status: 404 }),
+      scripts: new Response(null, { status: 500 })
+    };
+
+    expect(flowProjectState({ ...blank, response, error })).toBe(ERROR_API);
+  });
+
+  it("shows API issues if not responses are 404 and all errors are triggered", () => {
+    const error: PreflightChecklistErrors = {
+      every: true,
+      some: true,
+      findings: new Error("some error"),
+      pkg: new Error("some error"),
+      repo: new Error("some error"),
+      scripts: new Error("some error")
+    };
+
     expect(flowProjectState({ ...blank, error })).toBe(ERROR_UNKNOWN);
   });
 });
