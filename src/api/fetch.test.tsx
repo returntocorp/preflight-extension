763d707de2b728c63b2698466a627a4a8d20030e
Derrick Liu <dliu@lediur.com>
2018-11-19 14:51:39 -0800

    Add tests for fetch component factory

---
+++ src/api/fetch.test.tsx
@@ -0,0 +1,32 @@
+import { buildFetchComponent, RawApiFetch } from "@r2c/extension/api/fetch";
+import {
+  PermissionsResponse,
+  permissionsUrl
+} from "@r2c/extension/api/permissions";
+import { ExtractedRepoSlug, parseSlugFromUrl } from "@r2c/extension/utils";
+import { shallow } from "enzyme";
+import * as React from "react";
+
+describe("Fetch wrapper component builder", () => {
+  it("constructs a wrapper function as expected", () => {
+    const repoSlug: ExtractedRepoSlug = parseSlugFromUrl(
+      "https://github.com/returntocorp/secarta-extension"
+    );
+    const fakeChild = () => null;
+    const expected = (
+      <RawApiFetch<PermissionsResponse>
+        url={permissionsUrl(repoSlug)}
+        children={fakeChild}
+      />
+    );
+
+    const BuiltComponent = buildFetchComponent<PermissionsResponse>(
+      permissionsUrl
+    );
+    const actual = shallow(
+      <BuiltComponent children={fakeChild} repoSlug={repoSlug} />
+    );
+
+    expect(actual.contains(expected)).toBe(true);
+  });
+});
