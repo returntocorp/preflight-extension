8b54bb71e64acce8b613a2449c066945c31ae6cf
Tru Narla <trushithanarla@gmail.com>
2019-01-16 14:29:40 -0800

    added css for injector

--- src/content/github/PackageCriteriaInjector.tsx
+++ src/content/github/PackageCriteriaInjector.tsx
@@ -1,4 +1,8 @@
+import { Popover, Position } from "@blueprintjs/core";
+import { buildLineNoElemIdForBlobLine } from "@r2c/extension/content/github/BlobFindingsInjector";
+import DOMInjector from "@r2c/extension/content/github/DomInjector";
 import { ExtractedRepoSlug } from "@r2c/extension/utils";
+import * as classNames from "classnames";
 import * as React from "react";
 import "./PackageCriteriaInjector.css";
 
@@ -22,18 +26,23 @@ interface PackageCriteriaDetailsProps {
   repoSlug: ExtractedRepoSlug;
 }
 
-// interface CriteriaSpan {
-//   height: number;
-//   top: number;
-//   bottom: number;
-//   leftMargin: number;
-//   leftGutter: number;
-//   rightMargin: number;
-//   startGutterElem: Element;
-//   startCodeElem: Element;
-//   endGutterElem: Element;
-//   endCodeElem: Element;
-// }
+interface CriteriaSpan {
+  height: number;
+  top: number;
+  bottom: number;
+  leftMargin: number;
+  leftGutter: number;
+  rightMargin: number;
+  startGutterElem: Element;
+  startCodeElem: Element;
+  endGutterElem: Element;
+  endCodeElem: Element;
+}
+
+/*
+  TODO: write a fetch for criteria, that will fetch as we iterate through package names
+
+*/
 
 export default class PackageCriteriaHighlight extends React.PureComponent<
   PackageCriteriaDetailsProps
@@ -44,16 +53,83 @@ export default class PackageCriteriaHighlight extends React.PureComponent<
 
     console.log(packageInfos);
 
-    // const criteriaSpan = this.computeCriteriaSpan();
+    const criteriaSpan = this.computeCriteriaSpan(packageInfos);
 
-    // if (criteriaSpan == null || criteriaSpan.startGutterElem == null) {
-    //   return null;
-    // }
+    if (criteriaSpan == null || criteriaSpan.startGutterElem == null) {
+      return null;
+    }
 
-    return <></>;
+    return (
+      <DOMInjector
+        destination={criteriaSpan.startGutterElem}
+        childClassName="r2c-blob-finding-highlight-wrapper"
+        injectedClassName="r2c-blob-finding-highlight"
+        relation="direct"
+      >
+        <Popover
+          className="r2c-blob-finding-highlight-wrapper"
+          content={<>Hello</>}
+          position={Position.RIGHT_TOP}
+          minimal={true}
+          modifiers={{
+            preventOverflow: { boundariesElement: "viewport" },
+            offset: { offset: "0px,40px" }
+          }}
+          defaultIsOpen={false}
+        >
+          <div className="r2c-blob-finding-highlight-hitbox">
+            <div
+              className={classNames(
+                "finding-highlight-marker",
+                "marker-commit-match"
+              )}
+            />
+          </div>
+        </Popover>
+      </DOMInjector>
+    );
   }
 
-  // private computeCriteriaSpan = (): CriteriaSpan | null => {};
+  private computeCriteriaSpan = (
+    packageInfos: PackageInfo[]
+  ): CriteriaSpan | null => {
+    if (packageInfos) {
+      for (const packageInfo of packageInfos) {
+        if (packageInfo.startLine == null) {
+          return null;
+        }
+        const lineGutter = document.querySelector(
+          buildLineNoElemIdForBlobLine(packageInfo.startLine)
+        );
+        const lineCode = document.querySelector(
+          buildLineNoElemIdForBlobLine(packageInfo.startLine)
+        );
+
+        if (lineGutter == null || lineCode == null) {
+          return null;
+        }
+
+        const endLineBounds = lineGutter.getBoundingClientRect();
+        const startLineBounds = lineGutter.getBoundingClientRect();
+        const startCodeBounds = lineCode.getBoundingClientRect();
+
+        return {
+          height: endLineBounds.bottom - startLineBounds.top,
+          top: startLineBounds.top + window.scrollY,
+          bottom: endLineBounds.bottom + window.scrollY,
+          leftMargin: startLineBounds.left,
+          leftGutter: startLineBounds.right,
+          rightMargin: startCodeBounds.right,
+          startCodeElem: lineCode,
+          startGutterElem: lineGutter,
+          endCodeElem: lineCode,
+          endGutterElem: lineGutter
+        };
+      }
+    }
+
+    return null;
+  };
 
   private computePackageInfo = (): PackageInfo[] => {
     const packagejson = document.querySelectorAll(
@@ -70,7 +146,8 @@ export default class PackageCriteriaHighlight extends React.PureComponent<
           .textContent;
         if (
           packageName &&
-          !packageName.toUpperCase().includes("DEPENDENCIES")
+          !packageName.toUpperCase().includes("DEPENDENCIES") &&
+          code[0].getAttribute("data-line-number") != null
         ) {
           packageInfo.push({
             name: packageName.replace(/"/g, ""),
@@ -93,3 +170,9 @@ export default class PackageCriteriaHighlight extends React.PureComponent<
     return packageInfo;
   };
 }
+
+// class PackageCriteriaHighlighter extends React.PureComponent<
+//   PackageCriteriaHighlighterProps
+// > {
+//   public render() {}
+// }
